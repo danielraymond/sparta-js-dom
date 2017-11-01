455f0d105ddddfdc9c617fbcc0c958139f5e522c
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-11-01 15:51:00 +0000

    finished dom practice

--- the-dom-starter-code/index.js
+++ the-dom-starter-code/index.js
@@ -1,16 +1,28 @@
-var listItems = ["Go out","Come back" , "Buy food" , "Eat food"];
+var listItems = ["Go out", "Come back" , "Buy food" , "Eat food"];
 
 
 function addListItem(value) {
-
-  
+  var item = document.createElement("li");
+  var text = document.createTextNode(listItems[i]);
+  item.appendChild(text);
+  document.getElementById("list").appendChild(item);
 
 }
 
 // write append loop here
-
+for (var i = 0; i < listItems.length; i++) {
+  if (i % 2 === 0){
+    li.classList.add('even');
+    addListItem();
+  } 
+}
 
 // write the class loop here
 
+// for (var i = 0;) i < listItems.length; i ++) {
+//   if (i % 2 === 0) {
+//     li.classList.add('even');
+//   }
+// }
 
-
+console.log(document.body);
